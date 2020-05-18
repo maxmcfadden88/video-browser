86fb0721f69ba694b6631993d9a656161c5cd583
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-05-18 01:11:42 -0500

    first commit - added SearchBar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input v-on:input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function(event) {
+      console.log(event.target.value);
+    },
+  },
+};
+</script>
+
+<style></style>
