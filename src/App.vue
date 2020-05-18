4d4ef2f8cc499f75c02653b3f9ca3a70b11c09d9
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-05-18 02:07:39 -0500

    added axios, + YT apikey to App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-    <SearchBar></SearchBar>
+    <SearchBar @termChange="onTermChange"> </SearchBar>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+const API_KEY = "AIzaSyD4-YzT52xGiKHsOEoKFlZH7RRE7vKYZME";
 
 export default {
   name: "App",
 
   components: {
     SearchBar,
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm,
+          },
+        })
+        .then((response) => console.log(response));
+    },
   },
 };
 </script>
