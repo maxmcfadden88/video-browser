29832d2e04716a7a81c7ce5c6feb2d10320b5a4f
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-05-21 00:35:00 -0500

    added VideoListItem Component/installed bootstrap

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,15 +1,21 @@
 <template>
   <ul>
-    Video List
-    {{
-      videos.length
-    }}
+    <VideoListItem
+      v-for="video in videos"
+      :video="video"
+      :key="video.etag"
+    ></VideoListItem>
   </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem";
+
 export default {
   name: "VideoList",
+  components: {
+    VideoListItem,
+  },
   props: ["videos"],
 };
 </script>
